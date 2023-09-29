99924f7904589909999fedd6b45a8d74464a4558
Kylreia <rumbauakeith@gmail.com>
2023-09-29 17:13:46 +0100

    moving tank

--- themetaverse/tank1.gd
+++ themetaverse/tank1.gd
@@ -1,4 +1,13 @@
 extends CharacterBody3D
 
+@export var move_speed:float=4
+@export var rot_speed:float=2
+
 func _process(delta):
-	translate(Vector3.FORWARD * delta)
+	var move = Input.get_axis("move_backward", "move_forward")
+	
+	translate(Vector3.FORWARD * move * delta * move_speed)
+	
+	var turn = Input.get_axis("move_right", "move_left")
+	
+	rotate(Vector3.UP, turn * rot_speed * delta)
