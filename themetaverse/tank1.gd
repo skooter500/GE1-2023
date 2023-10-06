790f3b14da6a54021ac30a6ef650598a0dd5934b
Michael Cooper <C20321456@mytudublin.ie>
2023-10-06 15:21:46 +0100

    updated

--- themetaverse/tank1.gd
+++ themetaverse/tank1.gd
@@ -4,11 +4,11 @@ extends CharacterBody3D
 @export var rot_speed:float=1
 
 func _process(delta):	
-	var move = Input.get_axis("move_backwards", "move_forwards")
+	var move = Input.get_axis("move_forwards", "move_backwards")
 	print(move)
 	
 	translate(Vector3.FORWARD * move * delta * move_speed)
 	
-	var turn = Input.get_axis("turn_left", "turn_right")
+	var turn = Input.get_axis("turn_right", "turn_left")
 	
 	rotate(Vector3.UP, turn * rot_speed * delta)
